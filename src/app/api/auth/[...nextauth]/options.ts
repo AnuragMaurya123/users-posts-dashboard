28531be7f4ea8610666f615e2e4f36f41559d475
Anurag Maurya <anuragmaurya566@gmail.com>
2025-03-25 17:40:16 +0530

    some changes are done

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -9,6 +9,44 @@ import bcrypt from "bcryptjs";
 import GitHubProvider from "next-auth/providers/github";
 
  const authOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials:any){
+        await dbConnect();
+        if (!credentials.email || !credentials.password) {
+          throw new Error("Credentials not provided");
+        }
+        try {
+          const user = await Usermodel.findOne({
+            $or: [{ email: credentials.email }, { username: credentials.email }],
+          });
+          
+          if (!user) {
+            throw new Error("User not found with this email or username");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before logging in");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(String(credentials.password) , user.password);
+          if (!isPasswordCorrect) {
+            throw new Error("Incorrect Credentials");
+          }
+          return user as any;
+        } catch (error) {
+          // Throw the error directly instead of logging it
+          throw error;
+        }
+      },
+    }),
+   
+  ],
   pages: {
     signIn: "/sign-in",
     signOut: "/sign-out",
